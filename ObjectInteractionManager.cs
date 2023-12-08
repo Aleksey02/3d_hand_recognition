2205491b5ab8d2ef108b45ca1a37bfa4e6103b55
Aleksey02 <lyosha2002stich@gmail.com>
2023-12-08 20:52:55 +0500

    всё работает, добавил что при поднятии куба он не вращался и при отпускании дальше вращался

--- ObjectInteractionManager.cs
+++ ObjectInteractionManager.cs
@@ -34,23 +34,34 @@ public class ObjectInteractionManager : MonoBehaviour
 
     bool CheckCubeInteractionConditions()
     {
-         Debug.Log("Checking cube interaction conditions");
+        //Debug.Log("Checking cube interaction conditions");
         Vector3 handSphere4Pos = handTracking.handPoints[4].transform.position;
         Vector3 handSphere8Pos = handTracking.handPoints[8].transform.position;
 
         foreach (GameObject cube in cubes)
         {
-            float distanceToCube = Vector3.Distance(cube.transform.position, handSphere4Pos);
-            Debug.Log($"Distance to cube: {distanceToCube}");
-            Debug.Log($"Cube position: {cube.transform.position}");
-    Debug.Log($"Hand position (sphere 4): {handSphere4Pos}");
-            if (distanceToCube < cubeInteractionDistanceThreshold)
+            float distanceToCube1 = Vector3.Distance(cube.transform.position, handSphere4Pos);
+            float distanceToCube2 = Vector3.Distance(cube.transform.position, handSphere8Pos);
+            //Debug.Log($"Дистанция между кубом  и сферрой 1: {distanceToCube1} и сферой 2: {distanceToCube2}");
+            //Debug.Log($"Cube position: {cube.transform.position}");
+            //Debug.Log($"Hand position (sphere 4): {handSphere4Pos}");
+            Rigidbody cubeRigidbody = cube.GetComponent<Rigidbody>();
+            if (distanceToCube1 < cubeInteractionDistanceThreshold && distanceToCube2 < cubeInteractionDistanceThreshold)
             {
-                Debug.Log($"Interaction condition met for cube {cube.name}");
+                //Debug.Log($"куб находится на нужном расстоянии от обеих рук {cube.name}");
+        
+                // Проверяем, что компонент не равен null
+                if (cubeRigidbody != null)
+                {
+                    // Отключаем вращение по всем осям
+                    cubeRigidbody.freezeRotation = true;
+                }
                 return true;
+            }else if(cubeRigidbody!= null){
+                cubeRigidbody.freezeRotation = false;
             }
         }
-        Debug.Log("Interaction condition not met");
+        //Debug.Log("куб не находится на нужном расстоянии от обеих рук");
         return false;
     }
 
@@ -59,5 +70,40 @@ public class ObjectInteractionManager : MonoBehaviour
         Debug.Log("Move cubes");
         // Логика подъема кубов на основе данных отслеживания рук
         // Например, изменение позиции кубов в зависимости от движения рук
+        Vector3 handSphere4Pos = handTracking.handPoints[4].transform.position;
+    Vector3 handSphere8Pos = handTracking.handPoints[8].transform.position;
+
+    // Определить ближайший куб
+    GameObject closestCube = FindClosestCube(handSphere4Pos, handSphere8Pos);
+
+    if (closestCube != null)
+    {
+        // Вычислить вектор смещения между текущим положением рук и предыдущим положением
+        Vector3 offset = handSphere8Pos - handSphere4Pos;
+
+        // Переместить ближайший куб на вектор смещения
+        closestCube.transform.position = (handSphere4Pos + handSphere8Pos) / 2;
     }
+}
+
+GameObject FindClosestCube(Vector3 position1, Vector3 position2)
+{
+    GameObject closestCube = null;
+    float closestDistance = float.MaxValue;
+
+    foreach (GameObject cube in cubes)
+    {
+        float distanceToCube1 = Vector3.Distance(cube.transform.position, position1);
+        float distanceToCube2 = Vector3.Distance(cube.transform.position, position2);
+        float averageDistance = (distanceToCube1 + distanceToCube2) / 2;
+
+        if (averageDistance < closestDistance)
+        {
+            closestDistance = averageDistance;
+            closestCube = cube;
+        }
+    }
+
+    return closestCube;
+}
 }
